1a787c94ca462a3fed08673f1a983015144aca65
Christof Stocker <christof.stocker@aimc.at>
2015-10-21 01:18:48 +0200

    remove dummy code

--- src/prediction.jl
+++ src/prediction.jl
@@ -21,20 +21,15 @@ deriv(h::LinearPredictor, x::AbstractVector, w::AbstractVector) = x
 # ==========================================================================
 # h(x,w) = 1 / (1 + exp(-f(x,w))
 
+sigmoid(x) = 1 / (1 + exp(-x))
+
 immutable SigmoidPredictor{T<:Predictor} <: Predictor
   f::T
 end
-
-sigmoid(x) = 1 / (1 + exp(-x))
 
 SigmoidPredictor(f = LinearPredictor()) = SigmoidPredictor{typeof(f)}(f)
 value(h::SigmoidPredictor, x, w) = sigmoid(value(h.f, x, w))
 function deriv(h::SigmoidPredictor, x, w)
   z = value(h, x, w)
   z * (1 - z) * deriv(h.f, x, w)
-end
-
-function baseline(x, w)
-  z = sigmoid(dot(x, w))
-  z * (1 - z) * x
 end
