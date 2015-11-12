a9c54243244ffb3547cd6000e7e71d602b899fe8
Christof Stocker <christof.stocker@aimc.at>
2015-11-12 08:52:08 +0100

    implement risk efficiently

--- src/risk/riskspec.jl
+++ src/risk/riskspec.jl
@@ -36,31 +36,6 @@ end
   meanvalue(risk.loss, y, buffer)
 end
 
-@inline function grad{TPred<:Predictor, TLoss<:Loss, TPen<:NoPenalty}(
-    risk::RiskModel{TPred, TLoss, TPen},
-    X::AbstractMatrix,
-    w::AbstractArray,
-    y::AbstractArray,
-    ŷ::AbstractArray = value(risk.predictor, X, w))
-  dloss = deriv(risk.loss, y, ŷ)
-  dpred = grad(risk.predictor, X, w)
-  buffer = A_mul_Bt(dpred, dloss)
-  broadcast!(*, buffer, buffer, 1/length(y))
-end
-
-@inline function grad!{TPred<:Predictor, TLoss<:Loss, TPen<:NoPenalty}(
-    buffer::AbstractMatrix,
-    risk::RiskModel{TPred, TLoss, TPen},
-    X::AbstractMatrix,
-    w::AbstractArray,
-    y::AbstractArray,
-    ŷ::AbstractArray = value(risk.predictor, X, w))
-  dloss = deriv(risk.loss, y, ŷ)
-  dpred = grad(risk.predictor, X, w)
-  A_mul_Bt!(buffer, dpred, dloss)
-  broadcast!(*, buffer, buffer, 1/length(y))
-end
-
 # ==========================================================================
 # * generic predictor
 # * generic penalty
@@ -117,8 +92,44 @@ end
 
 # ==========================================================================
 # * linear predictor
-# * no penalty
-
-# ==========================================================================
-# * linear predictor
 # * generic penalty
+
+@inline function grad{INTERCEPT, TLoss<:Loss, TPen<:Penalty}(
+        risk::RiskModel{LinearPredictor{INTERCEPT}, TLoss, TPen},
+        X::AbstractMatrix,
+        w::AbstractVector,
+        y::AbstractArray,
+        ŷ::AbstractArray = value(risk.predictor, X, w))
+    buffer = zeros(length(w), 1)
+    grad!(buffer, risk, X, w, y, ŷ)
+end
+
+@inline function grad!{T, INTERCEPT, TLoss<:Loss, TPen<:Penalty}(
+        buffer::AbstractMatrix{T},
+        risk::RiskModel{LinearPredictor{INTERCEPT}, TLoss, TPen},
+        X::AbstractMatrix,
+        w::AbstractArray,
+        y::AbstractArray,
+        ŷ::AbstractArray = value(risk.predictor, X, w))
+    n = size(X, 2)
+    k = size(X, 1)
+    @_dimcheck length(y) == length(ŷ) == n && length(buffer) == length(w)
+    @_dimcheck length(buffer) == (INTERCEPT ? k+1 : k)
+    fill!(buffer, zero(T))
+    @inbounds for i = 1:n
+        dloss = deriv(risk.loss, y[i], ŷ[i])
+        for j = 1:k
+            tmp = dloss
+            tmp *= X[j, i]
+            tmp /= n
+            buffer[j] += tmp
+        end
+        if INTERCEPT
+            tmp = dloss
+            tmp *= risk.predictor.bias
+            tmp /= n
+            buffer[k+1] += tmp
+        end
+    end
+    addgrad!(buffer, risk.penalty, w, k)
+end
