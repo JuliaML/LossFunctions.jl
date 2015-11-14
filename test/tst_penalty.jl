858990bdecd2a2fdc5a7eb1f956974362e3392e5
Christof Stocker <christof.stocker@aimc.at>
2015-11-14 21:16:03 +0100

    add tests for penalty grad

--- test/tst_penalty.jl
+++ test/tst_penalty.jl
@@ -1,13 +1,39 @@
-function test_value(l::Penalty, f::Function)
-    msg2("$(l): ")
+function test_value(pen::Penalty, f::Function)
+    msg2("$(pen): ")
     for i = 1:20
         w = randn(10)
-        ref = value(l, w, length(w))::Float64
+        ref = value(pen, w, length(w))::Float64
         val = f(w)::Float64
         @test abs(ref - val) < 1e-10
-        ref = value(l, w, length(w)-1)::Float64
+        ref = value(pen, w, length(w)-1)::Float64
         val = f(w[1:(length(w)-1)])::Float64
         @test abs(ref - val) < 1e-10
+    end
+    println("ok")
+end
+
+function test_grad(pen::Penalty, g::Function)
+    msg2("$(pen): ")
+    for i = 1:20
+        len = 10
+        w = randn(len)
+        ref = grad(pen, w)::Vector{Float64}
+        buf = zeros(len)
+        for i = 1:len
+            buf[i] = deriv(pen, w[i])
+        end
+        val = g(w)::Vector{Float64}
+        @test sumabs(ref - val) < 1e-10 * length(w)
+        @test sumabs(buf - val) < 1e-10 * length(w)
+        ref = grad(pen, w, len-1)::Vector{Float64}
+        val = [g(w[1:(len-1)]); 0]::Vector{Float64}
+        @test sumabs(ref - val) < 1e-10 * length(w)
+        rnd = randn(len)
+        buf = copy(rnd)
+        ref = addgrad!(buf, pen, w)::Vector{Float64}
+        val = g(w)::Vector{Float64}
+        @test sumabs(ref - buf) < 1e-10 * length(w)
+        @test sumabs(ref - (rnd + val)) < 1e-10 * length(w)
     end
     println("ok")
 end
@@ -34,3 +60,25 @@ test_value(L2Penalty(0.1), _l2pen(0.1))
 test_value(L2Penalty(0.5), _l2pen(0.5))
 test_value(L2Penalty(1), _l2pen(1))
 test_value(L2Penalty(2), sumabs2)
+
+# ==========================================================================
+
+msg("Test gradient of penalty functions")
+
+function _l1pengrad(λ)
+    _grad(w) = λ * sign(w)
+    _grad
+end
+test_grad(L1Penalty(0.05), _l1pengrad(0.05))
+test_grad(L1Penalty(0.1), _l1pengrad(0.1))
+test_grad(L1Penalty(0.5), _l1pengrad(0.5))
+test_grad(L1Penalty(1), _l1pengrad(1))
+
+function _l2pengrad(λ)
+    _grad(w) = λ * w
+    _grad
+end
+test_grad(L2Penalty(0.05), _l2pengrad(0.05))
+test_grad(L2Penalty(0.1), _l2pengrad(0.1))
+test_grad(L2Penalty(0.5), _l2pengrad(0.5))
+test_grad(L2Penalty(1), _l2pengrad(1))
