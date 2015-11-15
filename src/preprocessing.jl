811288a55cd6ec723fe3fe81f6f929e0acc786f1
Christof Stocker <christof.stocker@aimc.at>
2015-11-15 19:39:44 +0100

    inbounds the center scale preprocessing loops

--- src/preprocessing.jl
+++ src/preprocessing.jl
@@ -1,7 +1,7 @@
 
 function center!(X::AbstractMatrix, μ::AbstractVector = vec(mean(X, 2)))
     k, n = size(X)
-    for j = 1:k
+    @inbounds for j = 1:k
         for i = 1:n
             X[j, i] = X[j, i] - μ[j]
         end
@@ -11,7 +11,7 @@ end
 
 function rescale!(X::AbstractMatrix, σ::AbstractVector = vec(std(X, 2)))
     k, n = size(X)
-    for j = 1:k
+    @inbounds for j = 1:k
         if σ[j] > 1
             for i = 1:n
                 X[j, i] = X[j, i] / σ[j]
@@ -23,7 +23,7 @@ end
 
 function center_rescale!(X::AbstractMatrix, μ::AbstractVector = vec(mean(X, 2)), σ::AbstractVector = vec(std(X, 2)))
     k, n = size(X)
-    for j = 1:k
+    @inbounds for j = 1:k
         for i = 1:n
             X[j, i] = X[j, i] - μ[j]
             if σ[j] > 1
