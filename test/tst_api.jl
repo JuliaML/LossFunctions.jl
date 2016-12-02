7dd53a2f7f5974ed7a0d36afed045121503d9605
Christof Stocker <c.stocker@hci-kdd.org>
2016-12-02 02:07:19 +0100

    add tests for vectorized deriv and deriv2

--- test/tst_api.jl
+++ test/tst_api.jl
@@ -18,18 +18,62 @@ function test_vector_value(l::DistanceLoss, t, y)
     end
 end
 
+function test_vector_deriv(l::MarginLoss, t, y)
+    @testset "$(l): " begin
+        ref = [ LossFunctions.deriv(l,t[i],y[i]) for i in 1:length(y) ]
+        @test LossFunctions.deriv.(l, t, y) == ref
+        @test t .* LossFunctions.deriv.(l, t .* y) == ref
+        @test l'.(t, y) == ref
+        @test t .* l'.(t .* y) == ref
+    end
+end
+
+function test_vector_deriv(l::DistanceLoss, t, y)
+    @testset "$(l): " begin
+        ref = [ LossFunctions.deriv(l,t[i],y[i]) for i in 1:length(y) ]
+        @test LossFunctions.deriv.(l, t, y) == ref
+        @test LossFunctions.deriv.(l, y - t) == ref
+        @test l'.(t, y) == ref
+        @test l'.(y - t) == ref
+    end
+end
+
+function test_vector_deriv2(l::MarginLoss, t, y)
+    @testset "$(l): " begin
+        ref = [ LossFunctions.deriv2(l,t[i],y[i]) for i in 1:length(y) ]
+        @test LossFunctions.deriv2.(l, t, y) == ref
+        @test LossFunctions.deriv2.(l, t .* y) == ref
+        @test l''.(t, y) == ref
+        @test l''.(t .* y) == ref
+    end
+end
+
+function test_vector_deriv2(l::DistanceLoss, t, y)
+    @testset "$(l): " begin
+        ref = [ LossFunctions.deriv2(l,t[i],y[i]) for i in 1:length(y) ]
+        @test LossFunctions.deriv2.(l, t, y) == ref
+        @test LossFunctions.deriv2.(l, y - t) == ref
+        @test l''.(t, y) == ref
+        @test l''.(y - t) == ref
+    end
+end
+
 @testset "Vectorized API" begin
     targets = rand([-1,1], 10)
     outputs = (rand(10)-.5) * 20
 
     for loss in margin_losses
         test_vector_value(loss, targets, outputs)
+        test_vector_deriv(loss, targets, outputs)
+        test_vector_deriv2(loss, targets, outputs)
     end
 
     targets = (rand(10)-.5) * 20
     outputs = (rand(10)-.5) * 20
     for loss in distance_losses
         test_vector_value(loss, targets, outputs)
+        test_vector_deriv(loss, targets, outputs)
+        test_vector_deriv2(loss, targets, outputs)
     end
 end
 
