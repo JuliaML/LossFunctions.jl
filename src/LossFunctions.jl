ed9e52598263b535acad63bf724f661756ae5343
Mihir Paradkar <mkp65@gmail.com>
2017-06-30 00:08:48 -0400

    Added ordinal margin losses

--- src/LossFunctions.jl
+++ src/LossFunctions.jl
@@ -46,6 +46,8 @@ export
     CrossentropyLoss,
     ZeroOneLoss,
 
+    OrdinalMarginLoss,
+
     weightedloss,
 
     AvgMode
---
+++ src/supervised/ordinal.jl
@@ -0,0 +1,28 @@
+struct OrdinalMarginLoss <: SupervisedLoss
+    loss::MarginLoss
+    nlevels::Int
+end
+
+for fun in (:value, :deriv, :deriv2)
+    @eval @fastmath function ($fun)(loss::OrdinalMarginLoss, target::Number, output::Number)
+        retval = 0
+        for t = 1:target - 1
+            retval += ($fun)(loss.loss, 1, output - t - 1)
+        end
+        for t = target + 1:loss.nlevels
+            retval += ($fun)(loss.loss, -1, output - t + 1)
+        end
+        retval
+    end
+end
+
+for prop in [:isminimizable, :isdifferentiable,
+             :istwicedifferentiable,
+             :isconvex, :isstrictlyconvex,
+             :isstronglyconvex, :isnemitski,
+             :isunivfishercons, :isfishercons,
+             :islipschitzcont, :islocallylipschitzcont,
+             :isclipable, :ismarginbased,
+             :isdistancebased]
+    @eval ($prop)(l::OrdinalMarginLoss) = ($prop)(l.loss)
+end
