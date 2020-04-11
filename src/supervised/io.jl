de622ac9cd48452f94eb60f4b06de2ce80e7ab27
Júlio Hoffimann <julio.hoffimann@gmail.com>
2020-04-11 17:14:36 -0300

    Improve yguide in plot recipe

--- src/supervised/io.jl
+++ src/supervised/io.jl
@@ -33,10 +33,11 @@ Base.print(io::IO, loss::WeightedBinaryLoss{T,W}, args...) where {T,W} = print(i
 
 _loss_xguide(loss::MarginLoss) = "y * h(x)"
 _loss_xguide(loss::DistanceLoss) = "h(x) - y"
+_loss_yguide(loss::SupervisedLoss) = "L("*_loss_xguide(loss)*")"
 
-@recipe function plot(loss::SupervisedLoss, range = -2:0.05:2; fun=value)
+@recipe function plot(loss::SupervisedLoss, range=-2:0.05:2; fun=value)
     xguide --> _loss_xguide(loss)
-    yguide --> "L(y, h(x))"
+    yguide --> _loss_yguide(loss)
     label  --> string(loss)
     l(a) = fun(loss, a)
     l, range
